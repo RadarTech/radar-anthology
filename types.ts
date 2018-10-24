ad42ab313f2e26f7bd169fd338b00c8e3aa4401e
Ian K Smith <smithki707@gmail.com>
2018-10-24 09:26:13 -0600

    Add TS enums for colors and breakpoints

---
+++ types.ts
@@ -0,0 +1,85 @@
+// --- Standard Breakpoints ------------------------------------------------- //
+
+export enum RadarBreakpointNames {
+  Tablet = 'tablet',
+  Desktop = 'desktop',
+  Widescreen = 'widescreen',
+  FullHD = 'fullhd',
+}
+
+export enum RadarBreakpointPxValues {
+  Tablet = '768px',
+  Desktop = '1140px',
+  Widescreen = '1280px',
+  FullHD = '1620px',
+}
+
+// --- Colors --------------------------------------------------------------- //
+
+export enum RadarBaseColorNames {
+  Light = 'light',
+  Dark = 'dark',
+  Orange = 'orange',
+  Red = 'red',
+  Green = 'green',
+}
+
+export enum RadarColorNames {
+  Black = 'black',
+  White = 'white',
+
+  LightBg = 'light-bg',
+  LightMg = 'light-mg',
+  Light = 'light',
+  LightFg = 'light-fg',
+
+  DarkBg = 'dark-bg',
+  DarkMg = 'dark-mg',
+  Dark = 'dark',
+  DarkFg = 'dark-fg',
+
+  OrangeBg = 'orange-bg',
+  OrangeMg = 'orange-mg',
+  Orange = 'orange',
+  OrangeFg = 'orange-fg',
+
+  RedBg = 'red-bg',
+  RedMg = 'red-mg',
+  Red = 'red',
+  RedFg = 'red-fg',
+
+  GreenBg = 'green-bg',
+  GreenMg = 'green-mg',
+  Green = 'green',
+  GreenFg = 'green-fg',
+}
+
+export enum RadarColorHexValues {
+  Black = '#000000',
+  White = '#ffffff',
+
+  LightBg = '#a5a5a5',
+  LightMg = '#dadada',
+  Light = '#f0f0f0',
+  LightFg = '#ffffff',
+
+  DarkBg = '#000000',
+  DarkMg = '#282828',
+  Dark = '#303030',
+  DarkFg = '#484848',
+
+  OrangeBg = '#dd5e17',
+  OrangeMg = '#f58912',
+  Orange = '#ff9810',
+  OrangeFg = '#ffac15',
+
+  RedBg = '#bf1717',
+  RedMg = '#df2525',
+  Red = '#ff3333',
+  RedFg = '#ff525e',
+
+  GreenBg = '#0d8c61',
+  GreenMg = '#26d986',
+  Green = '#33ff99',
+  GreenFg = '#a6ffbf',
+}
